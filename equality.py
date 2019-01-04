105a73f2123cfe659627c467d7ba46bcd333a11e
ntsmarkv <newtechstudios@gmail.com>
2019-01-04 09:29:01 -0800

    revisioned

--- equality.py
+++
@@ -1,8 +0,0 @@
-@bot.command(pass_context=True)
-async def equality(ctx, url):
-    channel = ctx.message.author.voice.voice_channel
-    await bot.join_voice_channel(channel)
-    server = ctx.message.server
-    player = voice.create_ffmpeg_player('music/voice/eg.wav')
-    players[server.id] = player
-    player.start()
